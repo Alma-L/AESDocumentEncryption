79ef161c6b23a9f02e340c72b084f7eaaf3e1068
Alma-L <alma.latifi1@student.uni-pr.edu>
2024-11-16 21:58:55 +0100

    Made a new example with PDF document

--- DocumentEncryption.java
+++ DocumentEncryption.java
@@ -1,64 +1,85 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
+import java.io.IOException;
+import java.security.SecureRandom;
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class DocumentEncryption {
-    private static final String key = "You're an idiot!";
     private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES";
+    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
+    private static final int KEY_SIZE = 256;
 
-    public static void encrypt(File inputFile) {
-        File encryptedFile = new File(inputFile.getAbsolutePath() + ".encrypted");
-        encryptToNewFile(inputFile, encryptedFile);
-        renameToOldFilename(inputFile, encryptedFile);
-    }
-
-    public static void decrypt(File inputFile) {
-        File decryptedFile = new File(inputFile.getAbsolutePath() + ".decrypted");
-        decryptToNewFile(inputFile, decryptedFile);
-        renameToOldFilename(inputFile, decryptedFile);
-    }
-
-    public static void decryptToNewFile(File input, File output) {
-        try (FileInputStream inputStream = new FileInputStream(input); FileOutputStream outputStream = new FileOutputStream(output)) {
-            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-            byte[] buff = new byte[1024];
-            for (int readBytes = inputStream.read(buff); readBytes > -1; readBytes = inputStream.read(buff)) {
-                outputStream.write(cipher.update(buff, 0, readBytes));
-            }
-            outputStream.write(cipher.doFinal());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private static final SecretKeySpec key = generateKey();
+    private static final IvParameterSpec iv = generateIv();
 
     public static void encryptToNewFile(File inputFile, File outputFile) {
-        try (FileInputStream inputStream = new FileInputStream(inputFile); FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        try (FileInputStream inputStream = new FileInputStream(inputFile);
+             FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] inputBytes = new byte[4096];
-            for (int n = inputStream.read(inputBytes); n > 0; n = inputStream.read(inputBytes)) {
-                byte[] outputBytes = cipher.update(inputBytes, 0, n);
-                outputStream.write(outputBytes);
+            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byte[] outputBytes = cipher.update(buffer, 0, bytesRead);
+                if (outputBytes != null) {
+                    outputStream.write(outputBytes);
+                }
             }
             byte[] outputBytes = cipher.doFinal();
-            outputStream.write(outputBytes);
+            if (outputBytes != null) {
+                outputStream.write(outputBytes);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void renameToOldFilename(File oldFile, File newFile) {
-        if (oldFile.exists()) {
-            oldFile.delete();
+    public static void decryptToNewFile(File inputFile, File outputFile) {
+        try (FileInputStream inputStream = new FileInputStream(inputFile);
+             FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(Cipher.DECRYPT_MODE, key, iv);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byte[] outputBytes = cipher.update(buffer, 0, bytesRead);
+                if (outputBytes != null) {
+                    outputStream.write(outputBytes);
+                }
+            }
+            byte[] outputBytes = cipher.doFinal();
+            if (outputBytes != null) {
+                outputStream.write(outputBytes);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        newFile.renameTo(oldFile);
+    }
+
+    private static SecretKeySpec generateKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+            keyGen.init(KEY_SIZE);
+            SecretKey secretKey = keyGen.generateKey();
+            return new SecretKeySpec(secretKey.getEncoded(), ALGORITHM);
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating key", e);
+        }
+    }
+
+    private static IvParameterSpec generateIv() {
+        byte[] ivBytes = new byte[16];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(ivBytes);
+        return new IvParameterSpec(ivBytes);
     }
 }
--- DocumentEncryptionTest.java
+++ DocumentEncryptionTest.java
@@ -1,62 +1,58 @@
 import java.io.File;
-import java.io.FileWriter;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 public class DocumentEncryptionTest {
     public static void main(String[] args) {
-        // Krijo një skedar me përmbajtje "SHIHEMI NE ROUTE 66 NE ORA 6"
-        File testFile = new File("testFile.txt");
-        try (FileWriter writer = new FileWriter(testFile)) {
-            writer.write("SHIHEMI NE ROUTE 66 NE ORA 6");  // Shkruaj përmbajtjen "SHIHEMI NE ROUTE 66 NE ORA 6"
-            System.out.println("Skedari origjinal u krijua me sukses!");
-        } catch (IOException e) {
-            e.printStackTrace();
+        File pdfFile = new File("C:\\Users\\Alma\\Desktop\\AESDocumentEncryption\\BookForEncryption.pdf");
+
+        if (!pdfFile.exists()) {
+            System.out.println("PDF file does not exist. Please ensure 'BookForEncryption.pdf' is in the specified directory.");
+            return;
         }
 
-        // Printoni përmbajtjen e skedarit të krijuar
-        System.out.println("Përmbajtja e skedarit origjinal:");
-        try (FileReader reader = new FileReader(testFile)) {
-            int content;
-            while ((content = reader.read()) != -1) {
-                System.out.print((char) content);  // Lexo dhe printo karakterin nga skedari
-            }
-            System.out.println();  // Shto një vijë të re pas përmbajtjes
-        } catch (IOException e) {
-            e.printStackTrace();
+        System.out.println("Original PDF file size: " + pdfFile.length() + " bytes");
+
+        File encryptedFile = new File("C:\\Users\\Alma\\Desktop\\AESDocumentEncryption\\BookForEncryption_encrypted.pdf");
+        File decryptedFile = new File("C:\\Users\\Alma\\Desktop\\AESDocumentEncryption\\BookForEncryption_decrypted.pdf");
+
+        // Encrypt the file
+        System.out.println("\nEncrypting the PDF file...");
+        DocumentEncryption.encryptToNewFile(pdfFile, encryptedFile);
+
+        if (encryptedFile.exists()) {
+            System.out.println("Encrypted file created: " + encryptedFile.getName());
+            System.out.println("Encrypted file size: " + encryptedFile.length() + " bytes");
+        } else {
+            System.out.println("Encryption failed!");
+            return;
         }
 
-        // Enkripto skedarin dhe krijo një skedar të ri të enkriptuar
-        System.out.println("\nDuke enkriptuar skedarin...");
-        File encryptedFile = new File("testFile_encrypted.txt"); // Emri i ri i skedarit të enkriptuar
-        DocumentEncryption.encryptToNewFile(testFile, encryptedFile);  // Enkriptimi dhe ruajtja në skedarin e ri
+        // Decrypt the file
+        System.out.println("\nDecrypting the PDF file...");
+        DocumentEncryption.decryptToNewFile(encryptedFile, decryptedFile);
 
-        // Kontrollo dhe printo përmbajtjen e skedarit të enkriptuar
-        System.out.println("Përmbajtja e skedarit të enkriptuar:");
-        try (FileReader reader = new FileReader(encryptedFile)) {
-            int content;
-            while ((content = reader.read()) != -1) {
-                System.out.print((char) content);  // Lexo dhe printo karakterin nga skedari i enkriptuar
-            }
-            System.out.println();  // Shto një vijë të re pas përmbajtjes
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (decryptedFile.exists()) {
+            System.out.println("Decrypted file created: " + decryptedFile.getName());
+            System.out.println("Decrypted file size: " + decryptedFile.length() + " bytes");
+        } else {
+            System.out.println("Decryption failed!");
+            return;
         }
 
-        // Dekripto skedarin dhe krijo një skedar të ri të dekriptuar
-        System.out.println("\nDuke dekriptuar skedarin...");
-        File decryptedFile = new File("testFile_decrypted.txt"); // Emri i ri i skedarit të dekriptuar
-        DocumentEncryption.decryptToNewFile(encryptedFile, decryptedFile);  // Dekriptimi dhe ruajtja në skedarin e ri
+        // Compare original and decrypted files
+        try {
+            byte[] originalBytes = Files.readAllBytes(pdfFile.toPath());
+            byte[] decryptedBytes = Files.readAllBytes(decryptedFile.toPath());
 
-        // Kontrollo dhe printo përmbajtjen e skedarit të dekriptuar
-        System.out.println("Përmbajtja e skedarit të dekriptuar:");
-        try (FileReader reader = new FileReader(decryptedFile)) {
-            int content;
-            while ((content = reader.read()) != -1) {
-                System.out.print((char) content);  // Lexo dhe printo karakterin nga skedari i dekriptuar
+            if (Arrays.equals(originalBytes, decryptedBytes)) {
+                System.out.println("\nTest Passed: Decrypted file matches the original file.");
+            } else {
+                System.out.println("\nTest Failed: Decrypted file does not match the original file.");
             }
-            System.out.println();  // Shto një vijë të re pas përmbajtjes
         } catch (IOException e) {
+            System.out.println("Error comparing files: " + e.getMessage());
             e.printStackTrace();
         }
     }
